439cd8348f33c936c3d8a44a0fcfc0aaba5c1d11
Lukasz Piliszczuk <lukasz.pili@gmail.com>
2011-12-06 16:33:24 +0100

    Step 5 : AuthenticateFilter and protect InsertSomeProductServlet.

---
+++ src/com/supinfo/supcommerce/filter/AuthenticateFilter.java
@@ -0,0 +1,41 @@
+package com.supinfo.supcommerce.filter;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class AuthenticateFilter implements Filter {
+
+	@Override
+	public void doFilter(ServletRequest req, ServletResponse resp, FilterChain filterChain) throws IOException,
+			ServletException {
+
+		HttpServletRequest httpReq = (HttpServletRequest) req;
+		HttpServletResponse httpResp = (HttpServletResponse) resp;
+
+		if (httpReq.getSession().getAttribute("username") == null) {
+			httpResp.sendRedirect(httpReq.getContextPath() + "/login.html");
+			return;
+		}
+
+		filterChain.doFilter(req, resp);
+	}
+
+	@Override
+	public void init(FilterConfig arg0) throws ServletException {
+
+	}
+
+	@Override
+	public void destroy() {
+
+	}
+
+}
