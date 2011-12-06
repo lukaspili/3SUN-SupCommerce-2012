994f55ede7ec9ac0eb5ca45bf6a1731f9899033a
Lukasz Piliszczuk <lukasz.pili@gmail.com>
2011-12-06 15:40:53 +0100

    Step 4 : LoginServlet and login.html.

---
+++ src/com/supinfo/supcommerce/servlet/LoginServlet.java
@@ -0,0 +1,26 @@
+package com.supinfo.supcommerce.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class LoginServlet extends HttpServlet {
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+		String username = req.getParameter("username");
+		String password = req.getParameter("password");
+
+		if (username == null || !username.equals("foo") || password == null || !password.equals("bar")) {
+			resp.sendRedirect(req.getContextPath() + "/login.html");
+			return;
+		}
+
+		req.getSession().setAttribute("username", username);
+		resp.sendRedirect(req.getContextPath() + "/listProduct");
+	}
+}
