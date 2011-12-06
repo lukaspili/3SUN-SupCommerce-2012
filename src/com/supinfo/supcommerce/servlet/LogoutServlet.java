5d7c826ca14131b021d580464950bfafc0a93ddd
Lukasz Piliszczuk <lukasz.pili@gmail.com>
2011-12-06 16:54:59 +0100

    Step 11 : LogoutServlet.

---
+++ src/com/supinfo/supcommerce/servlet/LogoutServlet.java
@@ -0,0 +1,25 @@
+package com.supinfo.supcommerce.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns = "/logout")
+public class LogoutServlet extends HttpServlet {
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		req.getSession().removeAttribute("username");
+		resp.sendRedirect(req.getContextPath() + "/login.html");
+	}
+	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		doGet(req, resp);
+	}
+
+}
